d4a87c65ed0c5a9602e85d2e5d5e7a1a7290e56e
Kristian Dimitrov <kriso8606@gmail.com>
2024-07-15 14:58:17 +0300

    Add files via upload

---
+++ main/app_driver.cpp
@@ -0,0 +1,80 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <esp_log.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <esp_matter.h>
+//#include "bsp/esp-bsp.h"
+
+#include <app_priv.h>
+
+static const char *TAG = "app_driver";
+extern uint16_t door_lock_endpoint_id;
+
+using namespace chip::app::Clusters;
+using namespace esp_matter;
+
+esp_err_t app_driver_light_set_defaults(uint16_t endpoint_id)
+{
+	 esp_err_t err = ESP_OK;
+    void *priv_data = endpoint::get_priv_data(endpoint_id);
+   // led_indicator_handle_t handle = (led_indicator_handle_t)priv_data;
+    node_t *node = node::get();
+    endpoint_t *endpoint = endpoint::get(node, endpoint_id);
+    cluster_t *cluster = NULL;
+    attribute_t *attribute = NULL;
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+
+    /* Setting brightness */
+    cluster = cluster::get(endpoint, DoorLock::Id);
+    attribute = attribute::get(cluster, DoorLock::Attributes::LockState::Id);
+    attribute::get_val(attribute, &val);
+    val.val.u8 = 1;
+    attribute::set_val(attribute, &val);
+     int value = val.val.u8;
+     gpio_hold_dis(GPIO_NUM); 
+     gpio_set_level(GPIO_NUM, false);
+    gpio_hold_en(GPIO_NUM); 
+                printf("SET_DEFAULT LockState %d\n",value);
+   // err |= app_driver_light_set_brightness(handle, &val);
+    return err;
+
+}
+
+esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
+                                      uint32_t attribute_id, esp_matter_attr_val_t *val)
+{
+    esp_err_t err = ESP_OK;
+    printf("PRE_UPDATE\n");
+     if (endpoint_id == door_lock_endpoint_id) {
+       // led_indicator_handle_t handle = (led_indicator_handle_t)driver_handle;
+        if (cluster_id == DoorLock::Id) {
+            if (attribute_id == DoorLock::Attributes::LockState::Id) {
+               // err = app_driver_light_set_power(handle, val);
+               int value = val->val.u8;
+               
+               if(value == 2){        
+               gpio_hold_dis(GPIO_NUM);      
+               	gpio_set_level(GPIO_NUM, true);
+               	gpio_hold_en(GPIO_NUM); 
+               }else{
+               gpio_hold_dis(GPIO_NUM); 
+                gpio_set_level(GPIO_NUM, false);
+                gpio_hold_en(GPIO_NUM); 
+               }
+                printf("LockState %d\n",value);
+            }
+        } 
+    }  
+    
+    return err;
+}
+
+
---
+++ main/app_priv.h
@@ -0,0 +1,45 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#pragma once
+
+#include <esp_err.h>
+#include <esp_matter.h>
+
+
+#define GPIO_NUM GPIO_NUM_2
+
+#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
+#include "esp_openthread_types.h"
+#endif
+
+typedef void *app_driver_handle_t;
+
+void door_lock_init();
+
+esp_err_t app_driver_light_set_defaults(uint16_t endpoint_id);
+
+esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
+                                      uint32_t attribute_id, esp_matter_attr_val_t *val);
+
+#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
+#define ESP_OPENTHREAD_DEFAULT_RADIO_CONFIG()                                           \
+    {                                                                                   \
+        .radio_mode = RADIO_MODE_NATIVE,                                                \
+    }
+
+#define ESP_OPENTHREAD_DEFAULT_HOST_CONFIG()                                            \
+    {                                                                                   \
+        .host_connection_mode = HOST_CONNECTION_MODE_NONE,                              \
+    }
+
+#define ESP_OPENTHREAD_DEFAULT_PORT_CONFIG()                                            \
+    {                                                                                   \
+        .storage_partition_name = "nvs", .netif_queue_size = 10, .task_queue_size = 10, \
+    }
+#endif
